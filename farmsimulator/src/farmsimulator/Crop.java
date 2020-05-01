51676236d06409c9ed4dd1521c2731d99244df8f
bpc33 <64391022+bpc33@users.noreply.github.com>
2020-05-02 11:17:23 +1200

    Crop

---
+++ farmsimulator/src/farmsimulator/Crop.java
@@ -0,0 +1,20 @@
+package farmsimulator;
+
+public class Crop extends Farm{
+
+	private float cropPurchasePrice = 0;
+	private float cropMoneyGiven = 0;
+
+	public Crop () {
+
+	}
+
+	public float getCropPurchasePrice() {
+		return cropPurchasePrice;
+	}
+
+	public float getCropMoneyGiven() {
+		return cropMoneyGiven;
+	}
+
+}
