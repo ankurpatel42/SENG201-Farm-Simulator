ecdca13e4086c40b54a39279a0b4e0e1af9586d4
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-03 14:29:53 +1200

    Update Farmer.java (3 May 2:29PM)

--- farmsimulator/src/farmsimulator/Farmer.java
+++ farmsimulator/src/farmsimulator/Farmer.java
@@ -3,11 +3,12 @@ package farmsimulator;
 public class Farmer {
 	
 	private String farmerName = null;
-	private int actionsLeft = 0;
 	private int farmerAge = 0;
+	private static int actionsLeft = 0;
+	private static int dayNum = 0;
 	
-	public Farmer () {
-		
+	public Farmer (String inputFarmerName) {
+		farmerName = inputFarmerName;
 	}
 	
 	public String getFarmerName() {
@@ -18,19 +19,36 @@ public class Farmer {
 		return actionsLeft;
 	}
 	
-	public void useAction() {
-		actionsLeft -= 1;
-	}
-	
 	public int getAge() {
 		return farmerAge;
 	}
 	
+	public static void useAction() {
+		actionsLeft--;
+	}
+	
+	public int getDayNum() {
+		return dayNum;
+	}
+	
+	public static void moveToNextDay() {
+		if (dayNum == GameEnvironment.getGameLength()) {
+			System.out.println("You cannot move to the next day as this is your last day.");
+		} else {
+			dayNum++;
+		}
+	}
+	
 	public void tendToFarmLand() {
 		useAction();
-		for (Crop crop : Farm.getCropsList()) {
+		for (Crop crop : Farm.getCropList()) {
 			crop.setDaysTillHarvest(crop.getDaysTillHarvest() - 1);
 		}
 		//All Animal Happiness Increases//
 	}
+	
+	public String toString() {
+		return "I'm a farmer and my name is " + farmerName;
+	}
+	
 }
