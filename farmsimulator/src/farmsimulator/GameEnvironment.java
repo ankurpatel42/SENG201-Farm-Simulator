d34110f0c16efb9abd5eb95e0ef4f962da4428ce
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-05 17:50:44 +1200

    Update GameEnvironment.java

--- farmsimulator/src/farmsimulator/GameEnvironment.java
+++ farmsimulator/src/farmsimulator/GameEnvironment.java
@@ -6,32 +6,84 @@ public class GameEnvironment {
 	
 	private static Scanner scanner = new Scanner(System.in);
 	private static int gameLength = 0;
+	private static Farmer farmer = null;
 	
-	public static void gameLengthChosen() {
+	public static int getGameLength() {
+		return gameLength;
+	}
+	
+	private static void farmType(int optionChosen) {
 		
-		String numberOfDaysChosen = scanner.nextLine();
+		Farm.setFarmType(optionChosen);
 		
-		while(true) {
-			try {
-				int numDays = Integer.parseInt(numberOfDaysChosen);
-				if (numDays < 5 || numDays > 10) {
-					System.out.print("Please enter a number between 5 and 10 (inclusive): ");
-					numberOfDaysChosen = scanner.nextLine();
-				} 
-				else {
-					gameLength = numDays;
-					break;
-				}
-				
-			} catch (NumberFormatException e) {
-				System.out.print("Please enter a number between 5 and 10 (inclusive): ");
-				numberOfDaysChosen = scanner.nextLine();
-			}
+		if (optionChosen == 1) {
+			Farm.setMoneyAvailible(100);
+			Farm.setCropGrowthRate(2);
+			Farm.setStartingAnimalHealthiness(4);
+			Farm.setStartingAnimalHappiness(3);
 		}
+		else if (optionChosen == 2) {
+			Farm.setMoneyAvailible(300);
+			Farm.setCropGrowthRate(3);
+			Farm.setStartingAnimalHealthiness(5);
+			Farm.setStartingAnimalHappiness(5);
+		}
+		else if (optionChosen == 3) {
+			Farm.setMoneyAvailible(500);
+			Farm.setCropGrowthRate(2);
+			Farm.setStartingAnimalHealthiness(7);
+			Farm.setStartingAnimalHappiness(6);
+		} 
+		else if (optionChosen == 4) {
+			Farm.setMoneyAvailible(2000);
+			Farm.setCropGrowthRate(3);
+			Farm.setStartingAnimalHealthiness(1);
+			Farm.setStartingAnimalHappiness(3);
+		}
+		else {
+			System.out.println("You have not entered a valid option.");
+		}
+		
 		
 	}
 	
-	public static String farmerNameChosen() {
+	private static void farmChosenValid() {
+		
+		String farmChosen = scanner.nextLine();
+		
+		//Infinite loop that terminates only once the user has entered a valid farm number according to specifications.
+		while(true) {
+			try {
+				int numFarm = Integer.parseInt(farmChosen);
+				if (numFarm < 1 || numFarm > 4) {
+					System.out.print("Please select a number between 1 and 4 (inclusive): ");
+					farmChosen = scanner.nextLine();
+				} 
+				else {
+					farmType(numFarm);
+					break;
+				}
+			} catch (NumberFormatException e) {
+				System.out.print("Please select a number between 1 and 4 (inclusive): ");
+				farmChosen = scanner.nextLine();
+			}
+		}
+		
+		System.out.println("You have selected option: " + farmChosen );
+	}
+	
+	private static void farmOptionsForPlayer() {
+		
+		System.out.println("What farm would you like to begin your journey on? Choose from the list below.");
+		System.out.println("1. Farm A");
+		System.out.println("2. Farm B");
+		System.out.println("3. Farm C");
+		System.out.println("4. Farm D");
+		System.out.print("Please enter a number to choose what farm you would like to work with: ");
+	}
+	
+	
+	private static String farmerNameChosen() {
 		
 		String farmerName = scanner.nextLine();
 		char[] farmerNameArray = farmerName.toCharArray();
@@ -64,113 +116,74 @@ public class GameEnvironment {
 		return farmerName;
 	}
 	
-	public static void farmChosenValid() {
-		
-		String farmChosen = scanner.nextLine();
-		
-		//Infinite loop that terminates only once the user has entered a valid farm number according to specifications.
-		while(true) {
-			try {
-				int numFarm = Integer.parseInt(farmChosen);
-				if (numFarm < 1 || numFarm > 4) {
-					System.out.print("Please select a number between 1 and 4 (inclusive): ");
-					farmChosen = scanner.nextLine();
-				} 
-				else {
-					farmType(numFarm);
-					break;
-				}
-			} catch (NumberFormatException e) {
-				System.out.print("Please select a number between 1 and 4 (inclusive): ");
-				farmChosen = scanner.nextLine();
-			}
-		}
-		
-		System.out.println("You have selected option: " + farmChosen );
-	}
 	
-	public static void farmOptionsForPlayer() {
-		
-		System.out.println("What farm would you like to begin your journey on? Choose from the list below.");
-		System.out.println("1. Farm A");
-		System.out.println("2. Farm B");
-		System.out.println("3. Farm C");
-		System.out.println("4. Farm D");
-		System.out.print("Please enter a number to choose what farm you would like to work with: ");
-	}
-	
-	
-	public static void farmType(int optionChosen) {
-		if (optionChosen == 1) {
-			Farm.setMoneyAvailible(100);
-			Farm.setCropGrowthRate(2);
-			Farm.setStartingAnimalHealthiness(4);
-			Farm.setStartingAnimalHappiness(3);
-		}
-		else if (optionChosen == 2) {
-			Farm.setMoneyAvailible(300);
-			Farm.setCropGrowthRate(3);
-			Farm.setStartingAnimalHealthiness(5);
-			Farm.setStartingAnimalHappiness(5);
-		}
-		else if (optionChosen == 3) {
-			Farm.setMoneyAvailible(500);
-			Farm.setCropGrowthRate(2);
-			Farm.setStartingAnimalHealthiness(7);
-			Farm.setStartingAnimalHappiness(6);
-		} 
-		else if (optionChosen == 4) {
-			Farm.setMoneyAvailible(20);
-			Farm.setCropGrowthRate(3);
-			Farm.setStartingAnimalHealthiness(1);
-			Farm.setStartingAnimalHappiness(3);
-		}
-		else {
-			System.out.println("You have not entered a valid option.");
-		}
-		
-		
-	}
-	
-	
-	public static void SetUpGame() {
-		
-		System.out.print("How many days would you like the game to last? (Choose between 5 and 10 days): ");
-		gameLengthChosen();
-		
-	}
-	
-	public static int getGameLength() {
-		return gameLength;
-	}
-	
-	public static void createFarm() {
+	private static void createFarm() {
 		
 		System.out.print("Give your farmer a name: ");
 		
 		String farmerName = farmerNameChosen();
 		
 		//instatiate farmer after user gives farmer their name
-		Farmer farmer = new Farmer(farmerName);
-		System.out.println(farmer);
+		farmer = new Farmer(farmerName);
 		
 		farmOptionsForPlayer();
 		
 		farmChosenValid();
+	}
+	
+	private static void SetUpGame() {
+		
+		System.out.print("How many days would you like the game to last? (Choose between 5 and 10 days): ");
+		
+		String numberOfDaysChosen = scanner.nextLine();
+		
+		while(true) {
+			try {
+				int numDays = Integer.parseInt(numberOfDaysChosen);
+				if (numDays < 5 || numDays > 10) {
+					System.out.print("Please enter a number between 5 and 10 (inclusive): ");
+					numberOfDaysChosen = scanner.nextLine();
+				} 
+				else {
+					gameLength = numDays;
+					break;
+				}
+				
+			} catch (NumberFormatException e) {
+				System.out.print("Please enter a number between 5 and 10 (inclusive): ");
+				numberOfDaysChosen = scanner.nextLine();
+			}
+		}
+		
+		createFarm();
+		
+	}
+	
+	
+	private static void MainGame() {
+		
+		System.out.print("Choose something: ");
+		int number = scanner.nextInt();
+		
+		while (true) {
+			if (number == 1) {
+				if (farmer.getDayNum() > gameLength) {
+					System.out.println("You have completed the game!");
+					break;
+				}
+				farmer.moveToNextDay();
+				System.out.print("Choose something: ");
+				number = scanner.nextInt();
+			} 
+		}
 		
 		scanner.close();
 	}
 	
-	public static void MainGame() {
-		
-	}
-		
 
 	public static void main(String[] args) {
 		SetUpGame();
-		createFarm();
 		MainGame();
-		
 	}
 
 }
