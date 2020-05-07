8ce6e88ac4f683028383e1790a0ef4df6ab55b5f
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-07 19:10:01 +1200

    Update Farm.java

--- farmsimulator/src/farmsimulator/Farm.java
+++ farmsimulator/src/farmsimulator/Farm.java
@@ -4,92 +4,70 @@ import java.util.ArrayList;
 
 public class Farm {
 	
-	private static String farmName = null;
-	private static double moneyAvailable = 0;
-	private static double cropGrowthRate = 0;
-	private static int farmTidiness = 0;
-	private static ArrayList<Animal> animalsList = null;
-	private static ArrayList<Crop> cropsList = null;
-	private static ArrayList<Item> itemsOwned = null;
-	private static String type = null;
+	private String farmName = null;
+	private double moneyAvailable = 0;
+	private double cropGrowthRate = 0;
+	private int farmTidiness = 0;
+	private ArrayList<Animal> animalsList = new ArrayList<Animal>();
+	private ArrayList<Crop> cropsList = new ArrayList<Crop>();
+	private ArrayList<Item> itemsOwned = new ArrayList<Item>();
+	private String type = null;
 	
-	public Farm(String name) {
+	
+	public Farm(String name, double money, double cropGrowth) {
 		farmName = name;
+		moneyAvailable = money;
+		cropGrowthRate = cropGrowth;
 	}
 	
-	public static String getFarmName() {
+	public String getFarmName() {
 		return farmName;
 	}
 	
-	public static void setMoneyAvailible(double newBalance) {
+	public void setMoneyAvailible(double newBalance) {
 		moneyAvailable += newBalance;
 	}
 	
-	public static double getMoneyAvailable() {
+	public double getMoneyAvailable() {
 		return moneyAvailable;
 	}
 	
 	
-	public static int getFarmTidiness() {
+	public int getFarmTidiness() {
 		return farmTidiness;
 	}
 	
-	public static void addAnimalToList(Animal animal) {
+	public void addAnimalToList(Animal animal) {
 		animalsList.add(animal);
 	}
 	
-	public static ArrayList<Animal> getAnimalList() {
+	public ArrayList<Animal> getAnimalList() {
 		return animalsList;
 	}
 	
-	public static void addCropToList(Crop crop) {
+	public void addCropToList(Crop crop) {
 		cropsList.add(crop);
 	}
 	
-	public static ArrayList<Crop> getCropList() {
+	public ArrayList<Crop> getCropList() {
 		return cropsList;
 	}
 	
-	public static void setCropGrowthRate(int option) {
-		if (cropGrowthRate == 0) {
-			cropGrowthRate = option;
-		} else {
-			System.out.println("You cannot change this value.");
-		}
-	}
-	
-	public static double getCropGrowthRate() {
+	public double getCropGrowthRate() {
 		return cropGrowthRate;
 	}
 	
-	public static void setFarmType(int option) {
-		if (type == null && option == 1) {
-			type = "Farm A";
-		} 
-		else if (type == null && option == 2) {
-			type = "Farm B";
-		}
-		else if (type == null && option == 3) {
-			type = "Farm C";
-		}
-		else if (type == null && option == 4) {
-			type = "Farm D";
-		}
-		else {
-			System.out.println("You cannot change this value.");
-		}
-	}
 	
-	public static String getFarmType() {
+	public String getFarmType() {
 		return type;
 	}
 	
 	
-	public static ArrayList<Item> getItemsOwned(){
+	public ArrayList<Item> getItemsOwned(){
 		return itemsOwned;
 	}
 	
-	public static void addNewItem(Item item){
+	public void addNewItem(Item item){
 		itemsOwned.add(item);
 	}
 
