a97165ed2a3b1578ec004f019423fe5c04c8e966
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-07 22:16:10 +1200

    Update Farmer.java

--- farmsimulator/src/farmsimulator/Farmer.java
+++ farmsimulator/src/farmsimulator/Farmer.java
@@ -12,6 +12,11 @@ public class Farmer {
 		farmerName = inputFarmerName;
 	}
 	
+	public Farmer (String inputFarmerName, int inputFarmerAge) {
+		farmerName = inputFarmerName;
+		farmerAge = inputFarmerAge;
+	}
+	
 	public String getFarmerName() {
 		return farmerName;
 	}
