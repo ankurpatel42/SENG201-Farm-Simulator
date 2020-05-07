18f3a14be974d0fd7ec4be67cb9383f7a6ad1c68
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-07 15:09:45 +1200

    Update FertiliserThree.java

--- farmsimulator/src/farmsimulator/FertiliserThree.java
+++ farmsimulator/src/farmsimulator/FertiliserThree.java
@@ -2,11 +2,7 @@ package farmsimulator;
 
 public class FertiliserThree extends Item {
 	
-	private int harvestSpeedUpTime = 5;
-	private double price = 400;
-	private String name = "F3";
-	
-	public int getHarvestSpeedUpTime() {
-		return harvestSpeedUpTime;
+	public FertiliserThree() {
+		super("F3", 400, 5);
 	}
 }
