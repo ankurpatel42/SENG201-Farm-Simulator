5424d6f84cb7377bbccdbb70e74e5a8318f215dd
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-07 14:49:37 +1200

    Update AnimalFoodTwo.java

--- farmsimulator/src/farmsimulator/AnimalFoodTwo.java
+++ farmsimulator/src/farmsimulator/AnimalFoodTwo.java
@@ -2,11 +2,7 @@ package farmsimulator;
 
 public class AnimalFoodTwo extends Item {
 	
-	private int happinessIncrease = 5;
-	private double price = 300;
-	private String name = "A2";
-	
-	public int getHappinessIncrease() {
-		return happinessIncrease;
+	public AnimalFoodTwo() {
+		super(300, 5, "A2");
 	}
 }
