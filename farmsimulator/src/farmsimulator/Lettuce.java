497714c3dbcd387c968614c72d8ba0931421b3a9
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-07 14:51:57 +1200

    Update Lettuce.java

--- farmsimulator/src/farmsimulator/Lettuce.java
+++ farmsimulator/src/farmsimulator/Lettuce.java
@@ -3,9 +3,6 @@ package farmsimulator;
 public class Lettuce extends Crop {
 
 	public Lettuce() {
-		name = "Lettuce"
-		cropPurchasePrice = 4;
-		cropMoneyGiven = 35;
-		daysTillHarvest = 5;
+		super("Lettuce", 4, 35, 5);
 	}
 }
