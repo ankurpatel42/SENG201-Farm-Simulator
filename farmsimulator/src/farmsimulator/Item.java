3341afa109dc76f4c7b3e5bfa1418ccbf94223e9
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-07 15:07:25 +1200

    Update Item.java

--- farmsimulator/src/farmsimulator/Item.java
+++ farmsimulator/src/farmsimulator/Item.java
@@ -4,6 +4,29 @@ public class Item {
 	
 	private double price;
 	private String name;
+	private int cropHarvestSpeedUpTime = 0;
+	private int animalHappinessIncrease = 0;
+	
+	// For Crop Items
+	public Item (String name, double price, int cropHarvestSpeedUpTime) {
+		this.name = name;
+		this.price = price;
+		this.cropHarvestSpeedUpTime = cropHarvestSpeedUpTime;
+	}
+	
+	//For Animal Items
+	public Item (double price, int increase, String name) {
+		this.price = price;
+		this.animalHappinessIncrease = increase;
+		this.name = name;
+	}
+	
+	
+	//For Water
+	public Item (String name, int increase) {
+		this.name = name;
+		this.cropHarvestSpeedUpTime = increase;
+	}
 	
 	public double getPrice() {
 		return price;
@@ -11,5 +34,14 @@ public class Item {
 	
 	public String getItemName() {
 		return name;
+	}
+
+	public int getHarvestSpeedUpTime() {
+		// TODO Auto-generated method stub
+		return cropHarvestSpeedUpTime;
+	}
+	
+	public int getAnimalHappinessIncrease() {
+		return animalHappinessIncrease;
 	}
 }
