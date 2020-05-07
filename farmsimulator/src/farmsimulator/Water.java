1e3b2f0a422f17b2dc0d3f68492bd9516deb6a20
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-07 14:55:18 +1200

    Update Water.java

--- farmsimulator/src/farmsimulator/Water.java
+++ farmsimulator/src/farmsimulator/Water.java
@@ -2,11 +2,7 @@ package farmsimulator;
 
 public class Water extends Item {
 	 
-	private int harvestSpeedUpTime = 1;
-	private double price = 0;
-	private String name = "Water";
-	
-	public int getHarvestSpeedUpTime() {
-		return harvestSpeedUpTime;
+	public Water () {
+		super("Water", 1);
 	}
 }
