d5aedea6d96820c683778244fec8c536894e49b9
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-12 19:39:14 +1200

    Update Farmer.java

--- farmsimulator/src/farmsimulator/Farmer.java
+++ farmsimulator/src/farmsimulator/Farmer.java
@@ -1,13 +1,10 @@
 package farmsimulator;
 
-import java.util.ArrayList;
-
 public class Farmer {
 	
 	private String farmerName = null;
 	private double bonus = 0;
 	private int actionsLeft = 2;
-	private ArrayList<Item> itemsOwned = new ArrayList<Item>();
 	
 	public Farmer (String inputFarmerName) {
 		farmerName = inputFarmerName;
@@ -24,14 +21,7 @@ public class Farmer {
 	public void useAction() {
 		actionsLeft--;
 	}
-	
-	public void additemToList(Item item) {
-		itemsOwned.add(item);
-	}
-	
-	public ArrayList<Item> getItemsOwned() {
-		return itemsOwned;
-	}
+
 	
 	/*
 	public int getDayNum() {
