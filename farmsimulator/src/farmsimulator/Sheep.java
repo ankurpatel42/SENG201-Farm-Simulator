d85d2e140fa08fb15d42532df7789e390f572fe8
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-12 18:48:18 +1200

    Update Sheep.java

--- farmsimulator/src/farmsimulator/Sheep.java
+++ farmsimulator/src/farmsimulator/Sheep.java
@@ -2,28 +2,12 @@ package farmsimulator;
 
 public class Sheep extends Animal {
 	
-	/*
-	public String sheepName = null;
-	public String sheepType = null;
-	public int sheepAge = 0;
-	*/
-	
 	public Sheep () {
 		super("Sheep", 100, 100, 100, 100);
 	}
 	
-	/*
-	public String getSheepName() {
-		return sheepName;
+	public String toString() {
+		return "Sheep: " + getAnimalName();
 	}
-	
-	public String getSheepType() {
-		return sheepType;
-	}
-	
-	public int getSheepAge() {
-		return sheepAge;
-	}
-	*/
 
 }
