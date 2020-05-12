6723b6ac9fb9e76a81393401b887fe024c7cef9e
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-12 18:46:48 +1200

    Update Cow.java

--- farmsimulator/src/farmsimulator/Cow.java
+++ farmsimulator/src/farmsimulator/Cow.java
@@ -2,29 +2,12 @@ package farmsimulator;
 
 public class Cow extends Animal {
 	
-	/*
-	public String cowName = null;
-	public String cowType = null;
-	public int cowAge = 0;
-	*/
-	
 	public Cow () {
 		super("Cow", 100, 100, 100, 100);
 	}
 	
-	//could add if we wanted
-	/*
-	public String getCowName() {
-		return cowName;
+	public String toString() {
+		return "Cow: " + getAnimalName();
 	}
-	
-	public String getCowType() {
-		return cowType;
-	}
-	
-	public int getCowAge() {
-		return cowAge;
-	}
-	*/
 
 }
