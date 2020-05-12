7b0c61038b377d188b4a0471a5200b6bd5befbd7
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-12 18:47:21 +1200

    Update Pig.java

--- farmsimulator/src/farmsimulator/Pig.java
+++ farmsimulator/src/farmsimulator/Pig.java
@@ -2,28 +2,13 @@ package farmsimulator;
 
 public class Pig extends Animal {
 	
-	/*
-	public String pigName = null;
-	public String pigType = null;
-	public int pigAge = 0;
-	*/
-	
 	public Pig () {
 		super("Pig", 100, 100, 100, 100);
 	}
 	
-	/*
-	public String getPigName() {
-		return pigName;
+	public String toString() {
+		return "Pig: " + getAnimalName();
 	}
-	
-	public String getPigType() {
-		return pigType;
-	}
-	
-	public int getPigAge() {
-		return pigAge;
-	}
-	*/
+
 
 }
