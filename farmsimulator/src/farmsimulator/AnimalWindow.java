e54bb07e4d574695f6d05b42725ae5259d3871cc
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-12 18:49:41 +1200

    Update AnimalWindow.java

--- farmsimulator/src/farmsimulator/AnimalWindow.java
+++ farmsimulator/src/farmsimulator/AnimalWindow.java
@@ -5,12 +5,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import java.awt.Color;
+import javax.swing.ListSelectionModel;
 
 public class AnimalWindow {
 
@@ -19,10 +23,14 @@ public class AnimalWindow {
 	private JFrame animalsFrame;
 	private JComboBox<String> animalSelection;
 	private String[] animalOptions = {"Cow", "Pig", "Sheep"};
-	private JComboBox<String> animalFoodSelection;
-	private String[] animalFoodSelectionOptions = {"Animal Food 1", "Animal Food 2", "Animal Food 3"};
 	DefaultComboBoxModel<String> animalOptionsModel = new DefaultComboBoxModel<String>(animalOptions);
-	DefaultComboBoxModel<String> animalFoodSelectionOptionsModel = new DefaultComboBoxModel<String>(animalFoodSelectionOptions);
+	
+	DefaultListModel<Animal> animalListModel = new DefaultListModel<Animal>();
+	JList<Animal> animalList = new JList<>(animalListModel);
+	
+	DefaultListModel<Item> animalFoodListModel = new DefaultListModel<Item>();
+	JList<Item> animalFoodList = new JList<>(animalFoodListModel);
+	
 	/**
 	 * Launch the application.
 	 */
@@ -66,13 +74,8 @@ public class AnimalWindow {
 		animalsFrame.getContentPane().setLayout(null);
 		
 		animalSelection = new JComboBox<>(animalOptionsModel);
-		animalSelection.setBounds(70, 60, 144, 22);
+		animalSelection.setBounds(70, 96, 144, 22);
 		animalsFrame.getContentPane().add(animalSelection);
-		
-		
-		animalFoodSelection = new JComboBox<>(animalFoodSelectionOptionsModel);
-		animalFoodSelection.setBounds(70, 124, 144, 22);
-		animalsFrame.getContentPane().add(animalFoodSelection);
 		
 		
 		JButton feedAnimalButton = new JButton("Feed Animal");
@@ -81,17 +84,38 @@ public class AnimalWindow {
 				if (game.getAnimalsOnFarm().size() == 0) {
 					JOptionPane.showMessageDialog(animalsFrame, "There are no Animals on your farm to feed yet");
 				}
+				else {
+					if (animalList.getSelectedValue() == null || animalFoodList.getSelectedValue() == null) {
+						JOptionPane.showMessageDialog(animalsFrame, "You must select an Animal and a food item to feed your Animal!");
+					}
+					else {
+						game.feedAnimal(animalList.getSelectedValue(), animalFoodList.getSelectedValue());
+						JOptionPane.showMessageDialog(animalsFrame, "You have fed " + animalList.getSelectedValue() + " " 
+								+ animalFoodList.getSelectedValue());
+					}
+;				}
 			}
 		});
-		feedAnimalButton.setBounds(70, 190, 144, 23);
+		feedAnimalButton.setBounds(401, 252, 144, 23);
 		animalsFrame.getContentPane().add(feedAnimalButton);
 		
 		JButton playWithAnimalButton = new JButton("Play with Animal");
-		playWithAnimalButton.setBounds(70, 252, 144, 23);
+		playWithAnimalButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (animalList.getSelectedValue() == null) {
+					JOptionPane.showMessageDialog(animalsFrame, "Select an animal to play with!");
+				}
+				else {
+					game.playWithAnima(animalList.getSelectedValue());
+					JOptionPane.showMessageDialog(animalsFrame, "You played with" + animalList.getSelectedValue());
+				}
+			}
+		});
+		playWithAnimalButton.setBounds(401, 286, 144, 23);
 		animalsFrame.getContentPane().add(playWithAnimalButton);
 		
 		JButton backToFarmButton = new JButton("Back to Farm");
-		backToFarmButton.setBounds(390, 303, 144, 23);
+		backToFarmButton.setBounds(470, 320, 144, 23);
 		backToFarmButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				closeAnimalWindow();
@@ -100,12 +124,48 @@ public class AnimalWindow {
 		});
 		animalsFrame.getContentPane().add(backToFarmButton);
 		
-		JLabel userFarmNameLabel = new JLabel("New label");
+		JLabel userFarmNameLabel = new JLabel("");
 		userFarmNameLabel.setBounds(205, 11, 132, 32);
+		userFarmNameLabel.setText(game.getFarmName());
 		animalsFrame.getContentPane().add(userFarmNameLabel);
 		
-		JLabel animalsLabel = new JLabel("Animals");
-		animalsLabel.setBounds(402, 11, 132, 32);
+		JLabel animalsLabel = new JLabel("Animals on Farm (pick an individual to feed!)");
+		animalsLabel.setBounds(372, 11, 278, 32);
 		animalsFrame.getContentPane().add(animalsLabel);
+		
+		
+		animalListModel.addAll(game.getAnimalsOnFarm());
+		
+		animalList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		animalList.setForeground(Color.WHITE);
+		animalList.setBackground(Color.GRAY);
+		animalList.setBounds(368, 45, 217, 198);
+		animalsFrame.getContentPane().add(animalList);
+		
+		JLabel feedAllAnimalsLabel = new JLabel("Feed and play with all Animals of a type!");
+		feedAllAnimalsLabel.setBounds(70, 54, 217, 32);
+		animalsFrame.getContentPane().add(feedAllAnimalsLabel);
+		
+		JLabel farmNameLabel = new JLabel("Farm:");
+		farmNameLabel.setBounds(152, 11, 62, 32);
+		animalsFrame.getContentPane().add(farmNameLabel);
+		
+		//animal food items owned
+		for(int i = 0; i < game.getItemsOwnedByFarmer().size(); i++) {
+			if (game.getItemsOwnedByFarmer().get(i) instanceof AnimalFoodOne || game.getItemsOwnedByFarmer().get(i) 
+					instanceof AnimalFoodTwo || game.getItemsOwnedByFarmer().get(i) instanceof AnimalFoodThree) {
+				animalFoodListModel.add(i, game.getItemsOwnedByFarmer().get(i));
+			}
+		}
+		
+		animalFoodList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		animalFoodList.setForeground(Color.WHITE);
+		animalFoodList.setBackground(Color.GRAY);
+		animalFoodList.setBounds(70, 177, 217, 109);
+		animalsFrame.getContentPane().add(animalFoodList);
+		
+		JLabel animalFoodItemsLabel = new JLabel("Animal Food Items available");
+		animalFoodItemsLabel.setBounds(70, 134, 162, 32);
+		animalsFrame.getContentPane().add(animalFoodItemsLabel);
 	}
 }
