0453a17a29e3ab0259a723be91a80f630cdc31a6
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-13 19:27:04 +1200

    Update Sheep.java (clean up code)

--- farmsimulator/src/farmsimulator/Sheep.java
+++ farmsimulator/src/farmsimulator/Sheep.java
@@ -2,8 +2,8 @@ package farmsimulator;
 
 public class Sheep extends Animal {
 	
-	public Sheep () {
-		super("Sheep", 75, 0, 0, 0);
+	public Sheep(String nameForSheep, int initialHappiness, int initialHealthiness) {
+		super(nameForSheep, 50, 0, initialHappiness, initialHealthiness);
 	}
 	
 	public String toString() {
