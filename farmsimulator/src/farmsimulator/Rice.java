a42cd27254c068adc0677914d342e7ea9d42fee6
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-14 17:01:32 +1200

    finalscreen / code changes

--- farmsimulator/src/farmsimulator/Rice.java
+++ farmsimulator/src/farmsimulator/Rice.java
@@ -2,7 +2,7 @@ package farmsimulator;
 
 public class Rice extends Crop {
 	
-	public Rice(GameEnvironment game) {
-		super("Rice", 2, 20, 5, game);
+	public Rice() {
+		super("Rice", 2, 20, 5);
 	}
 }
