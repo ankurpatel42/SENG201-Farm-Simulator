7f0a1e0b4e6d9c5e5b951319ed2ec688d3e1d511
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-14 16:59:40 +1200

    finalscreen / code changes

--- farmsimulator/src/farmsimulator/Corn.java
+++ farmsimulator/src/farmsimulator/Corn.java
@@ -2,7 +2,8 @@ package farmsimulator;
 
 public class Corn extends Crop {
 	
-	public Corn(GameEnvironment game) {
-		super("Corn", 5, 60, 5, game);
+	public Corn() {
+		super("Corn", 5, 60, 5);
 	}
+	
 }
