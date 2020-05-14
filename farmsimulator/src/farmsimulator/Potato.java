e6684f238cfe7347233499d682c675674571e03a
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-14 17:00:45 +1200

    finalscreen / changes

--- farmsimulator/src/farmsimulator/Potato.java
+++ farmsimulator/src/farmsimulator/Potato.java
@@ -2,7 +2,7 @@ package farmsimulator;
 
 public class Potato extends Crop {
 	
-	public Potato(GameEnvironment game) {
-		super("Potato", 4, 50, 8, game);
+	public Potato() {
+		super("Potato", 4, 50, 8);
 	}
 }
