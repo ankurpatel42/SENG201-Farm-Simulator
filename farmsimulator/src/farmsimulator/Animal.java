59b52441110f9261642d2a4209c1384196d0d260
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-14 16:56:18 +1200

    final screen / code changes

--- farmsimulator/src/farmsimulator/Animal.java
+++ farmsimulator/src/farmsimulator/Animal.java
@@ -7,17 +7,15 @@ public abstract class Animal {
 	private double animalHealthiness = 0;
 	private double animalHappiness = 0;
 	private String name;
-	private GameEnvironment game;
 	private String message;
 	
 	
-	public Animal (String name, double animalPurchasePrice, double animalMoneyGiven, double animalHealthiness, double animalHappiness, GameEnvironment game) {
+	public Animal (String name, double animalPurchasePrice, double animalMoneyGiven, double animalHealthiness, double animalHappiness) {
 		this.animalPurchasePrice = animalPurchasePrice;
 		this.animalMoneyGiven = animalMoneyGiven;
 		this.animalHealthiness = animalHealthiness;
 		this.animalHappiness = animalHappiness;
 		this.name = name;
-		this.game = game;
 	}
 	
 	public double getAnimalPurchasePrice() {
@@ -36,7 +34,7 @@ public abstract class Animal {
 		return animalMoneyGiven;
 	}
 	
-	public String feedAnimal(Item choice) {
+	public String feedAnimal(Item choice, GameEnvironment game) {
 		if (game.useFarmerAction() == true) {
 			animalHealthiness += choice.getAnimalHappinessIncrease();
 			game.getItemsOwnedByFarmer().remove(choice);
@@ -48,7 +46,7 @@ public abstract class Animal {
 		return message;
 	}
 	
-	public String playWithAnimal() {
+	public String playWithAnimal(GameEnvironment game) {
 		if (game.useFarmerAction() == true) {
 			animalHappiness += 3;
 			message = "You have played with " + name;
