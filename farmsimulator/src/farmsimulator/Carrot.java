4e6cb853b4b67d3e096f0eea575ee62eabecb75a
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-14 16:58:09 +1200

    finalscreen / code changes

--- farmsimulator/src/farmsimulator/Carrot.java
+++ farmsimulator/src/farmsimulator/Carrot.java
@@ -2,8 +2,8 @@ package farmsimulator;
 
 public class Carrot extends Crop {
 	
-	public Carrot(GameEnvironment game) {
-		super("Carrot", 10, 100, 5, game);
+	public Carrot() {
+		super("Carrot", 10, 100, 5);
 	}
 	
 }
