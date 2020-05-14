5864c12cbdda6417bbdb63a4036418ad91f002e8
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-14 17:02:09 +1200

    final screen / code changes

--- farmsimulator/src/farmsimulator/Wheat.java
+++ farmsimulator/src/farmsimulator/Wheat.java
@@ -2,7 +2,7 @@ package farmsimulator;
 
 public class Wheat extends Crop {
 
-	public Wheat (GameEnvironment game) {
-		super("Wheat", 2, 10, 3, game);
+	public Wheat () {
+		super("Wheat", 2, 10, 3);
 	}
 }
