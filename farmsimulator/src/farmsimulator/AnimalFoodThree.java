5b4826c9eff665edf771f4737624e377a2a4d27c
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:24:55 +1200

    comments

--- farmsimulator/src/farmsimulator/AnimalFoodThree.java
+++ farmsimulator/src/farmsimulator/AnimalFoodThree.java
@@ -1,7 +1,18 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for an AnimalFoodThree object. This class extends the Item Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
 public class AnimalFoodThree extends Item {
 	
+	/**
+	 * AnimalFoodThree constructor. This class creates an instance of AnimalFoodThree by providing initial values to its parent class' constructor.
+	 */
 	public AnimalFoodThree() {
 		super(700, 10, "A3");
 	}
