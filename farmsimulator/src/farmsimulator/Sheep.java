99f044410f94cc59b54e69d9fc41dcb3e191a28d
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:37:05 +1200

    comments

--- farmsimulator/src/farmsimulator/Sheep.java
+++ farmsimulator/src/farmsimulator/Sheep.java
@@ -1,11 +1,25 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for a Sheep object. This class extends the Animal Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
 public class Sheep extends Animal {
 	
+	/**
+	 * Sheep constructor. This class creates an instance of Sheep by providing initial values to its parent class' constructor.
+	 */
 	public Sheep(String nameForSheep, int initialHappiness, int initialHealthiness) {
 		super(nameForSheep, 50, 0, initialHappiness, initialHealthiness);
 	}
 	
+	/**
+	 * String representation of a Sheep object.
+	 */
 	public String toString() {
 		return getAnimalName() + " (Sheep)    :     " + getAnimalHealthiness() + "     :     " + getAnimalHappiness();
 	}
