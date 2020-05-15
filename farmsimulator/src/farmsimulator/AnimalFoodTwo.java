894051e6b5104499704b74b4227aad39db50318b
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:25:41 +1200

    comments

--- farmsimulator/src/farmsimulator/AnimalFoodTwo.java
+++ farmsimulator/src/farmsimulator/AnimalFoodTwo.java
@@ -1,7 +1,18 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for an AnimalFoodTwo object. This class extends the Item Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
 public class AnimalFoodTwo extends Item {
 	
+	/**
+	 * AnimalFoodTwo constructor. This class creates an instance of AnimalFoodTwo by providing initial values to its parent class' constructor.
+	 */
 	public AnimalFoodTwo() {
 		super(300, 5, "A2");
 	}
