844d5bc4e320ab14e59c659d99689a3c71a86041
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:27:42 +1200

    comments

--- farmsimulator/src/farmsimulator/Corn.java
+++ farmsimulator/src/farmsimulator/Corn.java
@@ -1,7 +1,18 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for a Corn object. This class extends the Crop Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
 public class Corn extends Crop {
 	
+	/**
+	 * Corn constructor. This class creates an instance of Corn by providing initial values to its parent class' constructor.
+	 */
 	public Corn() {
 		super("Corn", 5, 60, 5);
 	}
