199f907e1ea82440553d825b877e28e04f89dab4
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:27:16 +1200

    comments

--- farmsimulator/src/farmsimulator/Carrot.java
+++ farmsimulator/src/farmsimulator/Carrot.java
@@ -1,7 +1,19 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for a Carrot object. This class extends the Crop Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
+
 public class Carrot extends Crop {
 	
+	/**
+	 * Carrot constructor. This class creates an instance of Carrot by providing initial values to its parent class' constructor.
+	 */
 	public Carrot() {
 		super("Carrot", 10, 100, 5);
 	}
