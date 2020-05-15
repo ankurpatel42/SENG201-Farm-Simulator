e754c478465f4a48ae2f510e259df7937250499a
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:37:59 +1200

    comments

--- farmsimulator/src/farmsimulator/Water.java
+++ farmsimulator/src/farmsimulator/Water.java
@@ -1,7 +1,18 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for a Water object. This class extends the Item Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
 public class Water extends Item {
 	 
+	/**
+	 * Water constructor. This class creates an instance of Water by providing initial values to its parent class' constructor.
+	 */
 	public Water () {
 		super("Water", 1);
 	}
