b14d3d6d18a4bcf8963a7739e104dbbae739b7f8
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:34:09 +1200

    comments

--- farmsimulator/src/farmsimulator/Potato.java
+++ farmsimulator/src/farmsimulator/Potato.java
@@ -1,7 +1,18 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for a Potato object. This class extends the Crop Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
 public class Potato extends Crop {
 	
+	/**
+	 * Potato constructor. This class creates an instance of Potato by providing initial values to its parent class' constructor.
+	 */
 	public Potato() {
 		super("Potato", 4, 50, 8);
 	}
