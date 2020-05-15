7e719224a59eb8a4f1be91c9e85b61af01e1e4b9
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:28:07 +1200

    comments

--- farmsimulator/src/farmsimulator/Cow.java
+++ farmsimulator/src/farmsimulator/Cow.java
@@ -1,11 +1,25 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for a Cow object. This class extends the Animal Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
 public class Cow extends Animal {
 	
+	/**
+	 * Cow constructor. This class creates an instance of Cow by providing initial values to its parent class' constructor.
+	 */
 	public Cow (String nameForCow, int initialHappiness, int initialHealthiness) {
 		super(nameForCow, 100, 0, initialHappiness, initialHealthiness);
 	}
 	
+	/**
+	 * String representation of a Cow object.
+	 */
 	public String toString() {
 		return getAnimalName() + " (Cow)    :     " + getAnimalHealthiness() + "     :     " + getAnimalHappiness();
 	}
