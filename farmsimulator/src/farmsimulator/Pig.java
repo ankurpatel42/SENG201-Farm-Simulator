a3296bc3780160ef38858dbd78bcc0664cb42772
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:33:43 +1200

    comments

--- farmsimulator/src/farmsimulator/Pig.java
+++ farmsimulator/src/farmsimulator/Pig.java
@@ -1,11 +1,25 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for a Pig object. This class extends the Animal Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
 public class Pig extends Animal {
 	
+	/**
+	 * Pig constructor. This class creates an instance of Pig by providing initial values to its parent class' constructor.
+	 */
 	public Pig (String nameForPig, int initialHappiness, int initialHealthiness) {
 		super(nameForPig, 75, 0, initialHappiness, initialHealthiness);
 	}
 	
+	/**
+	 * String representation of a Pig object.
+	 */
 	public String toString() {
 		return getAnimalName() + " (Pig)    :     " + getAnimalHealthiness() + "     :     " + getAnimalHappiness();
 	}
