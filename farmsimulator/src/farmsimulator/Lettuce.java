6685ec0047baa19c83aae598a9eca5fba27a7736
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:32:53 +1200

    comments

--- farmsimulator/src/farmsimulator/Lettuce.java
+++ farmsimulator/src/farmsimulator/Lettuce.java
@@ -1,7 +1,18 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for a Lettuce object. This class extends the Crop Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
 public class Lettuce extends Crop {
 
+	/**
+	 * Lettuce constructor. This class creates an instance of Lettuce by providing initial values to its parent class' constructor.
+	 */
 	public Lettuce() {
 		super("Lettuce", 4, 35, 5);
 	}
