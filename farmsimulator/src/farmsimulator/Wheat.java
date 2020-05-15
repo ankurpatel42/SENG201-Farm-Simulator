bc715b178b89fd8f6541f9772634c787180e5121
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:38:23 +1200

    comments

--- farmsimulator/src/farmsimulator/Wheat.java
+++ farmsimulator/src/farmsimulator/Wheat.java
@@ -1,7 +1,19 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for a Wheat object. This class extends the Crop Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
+
 public class Wheat extends Crop {
 
+	/**
+	 * Wheat constructor. This class creates an instance of Wheat by providing initial values to its parent class' constructor.
+	 */
 	public Wheat () {
 		super("Wheat", 2, 10, 3);
 	}
