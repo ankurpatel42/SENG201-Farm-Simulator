32aa2a8dc22389815d3d52059c6ab2cf16ca9e07
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:32:29 +1200

    comments

--- farmsimulator/src/farmsimulator/Item.java
+++ farmsimulator/src/farmsimulator/Item.java
@@ -1,49 +1,107 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for the Item objects.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
 public abstract class Item {
 	
+	/**
+	 * The price of the item.
+	 */
 	private double price = 0;
+	/**
+	 * The name of the item.
+	 */
 	private String name = null;
+	/**
+	 * The amount the item speeds up crop harvest time.
+	 */
 	private int cropHarvestSpeedUpTime = 0;
+	/**
+	 * The amount the item increases animal happiness.
+	 */
 	private int animalHappinessIncrease = 0;
 	
-	// For Crop Items
+	/**
+	 * Item constructor for crop items.
+	 * 
+	 * @param name						The name of the item.
+	 * @param price						The price of the item.
+	 * @param cropHarvestSpeedUpTime	The crop harvest speed up time of the item.
+	 */
 	public Item (String name, double price, int cropHarvestSpeedUpTime) {
 		this.name = name;
 		this.price = price;
 		this.cropHarvestSpeedUpTime = cropHarvestSpeedUpTime;
 	}
 	
-	//For Animal Items
+	/**
+	 * Item constructor for animal items.
+	 * 
+	 * @param price					The price of the item.
+	 * @param increase				The animal happiness increase of the item.
+	 * @param name					The name of the item.
+	 */
 	public Item (double price, int increase, String name) {
 		this.price = price;
 		this.animalHappinessIncrease = increase;
 		this.name = name;
 	}
 	
-	//For Water
+	/**
+	 * Item constructor for a water item.
+	 * 
+	 * @param name				The name of the item.
+	 * @param increase			The crop harvest speed up time of the item.
+	 */
 	public Item (String name, int increase) {
 		this.name = name;
 		this.cropHarvestSpeedUpTime = increase;
 	}
 	
+	/**
+	 * String representation of item objects.
+	 */
 	public String toString() {
 		return name;
 	}
 	
+	/**
+	 * Gets the price of the item.
+	 * 
+	 * @return				A double which is the price of the item.
+	 */
 	public double getPrice() {
 		return price;
 	}
 	
+	/**
+	 * Gets the name of the item.
+	 * 
+	 * @return				A string which is the name of the item.
+	 */
 	public String getItemName() {
 		return name;
 	}
 
+	/**
+	 * Gets the crop harvest speed up tome of the item.
+	 * 
+	 * @return				An integer which is the crop harvest speed up time of the item.
+	 */
 	public int getHarvestSpeedUpTime() {
-		// TODO Auto-generated method stub
 		return cropHarvestSpeedUpTime;
 	}
 	
+	/**
+	 * Gets the animal happiness increase of the item.
+	 * 
+	 * @return				An integer which is the animal happiness increase of the item.
+	 */
 	public int getAnimalHappinessIncrease() {
 		return animalHappinessIncrease;
 	}
