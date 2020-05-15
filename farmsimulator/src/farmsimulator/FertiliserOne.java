ba58b06e48172e2cd6070adb781817573203cffc
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:30:08 +1200

    comments

--- farmsimulator/src/farmsimulator/FertiliserOne.java
+++ farmsimulator/src/farmsimulator/FertiliserOne.java
@@ -1,7 +1,18 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for a FertiliserOne object. This class extends the Item Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
 public class FertiliserOne extends Item {
 	
+	/**
+	 * FertiliserOne constructor. This class creates an instance of FertiliserOne by providing initial values to its parent class' constructor.
+	 */
 	public FertiliserOne() {
 		super("F1", 100, 1);
 	}
