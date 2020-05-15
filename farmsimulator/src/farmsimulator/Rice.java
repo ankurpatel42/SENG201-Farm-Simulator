4baf257af607d3b3710d96340b40d121dd39e5f8
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-15 19:36:17 +1200

    comments

--- farmsimulator/src/farmsimulator/Rice.java
+++ farmsimulator/src/farmsimulator/Rice.java
@@ -1,7 +1,19 @@
 package farmsimulator;
 
+/** 
+ * This class contains attributes and methods for a Rice object. This class extends the Crop Class.
+ * 
+ * @author Ankur Patel, Benjamin Crozier
+ * @version 15/05/2020
+ */
+
+
+
 public class Rice extends Crop {
 	
+	/**
+	 * Rice constructor. This class creates an instance of Rice by providing initial values to its parent class' constructor.
+	 */
 	public Rice() {
 		super("Rice", 2, 20, 5);
 	}
