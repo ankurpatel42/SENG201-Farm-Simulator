5d7e4bf316215cedcacafd85cf4fb4de7c89c80a
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-16 12:30:21 +1200

    comments / code updates

--- farmsimulator/src/farmsimulator/GeneralStore.java
+++ farmsimulator/src/farmsimulator/GeneralStore.java
@@ -38,18 +38,18 @@ public class GeneralStore {
 	 * Purchasing an item.
 	 * 
 	 * @param item			The item to be bought.
-	 * @param game			GameEnvironment object.
+	 * @param farm			The farm which the item will be placed on.
 	 * @return					A string which states whether the item purchase has been successfull or not.
 	 */
-	public String purchaseItem(Item item, GameEnvironment game) {
-		money = game.getFarm().getMoneyAvailable();
+	public String purchaseItem(Item item, Farm farm) {
+		money = farm.getMoneyAvailable();
 		if (money < item.getPrice()){
 			message = "Sorry, You have insuffcient funds to purchase this item";
 		}
 		else {
 			money = money - item.getPrice();
-			game.getFarm().setMoneyAvailable(money);
-			game.getFarm().addNewItem(item);
+			farm.setMoneyAvailable(money);
+			farm.addNewItem(item);
 			message = "You have purchased one " + item.getItemName();
 		}
 		return message;
@@ -59,18 +59,18 @@ public class GeneralStore {
 	 * Purchasing an animal.
 	 * 
 	 * @param animal		The animal to be bought.
-	 * @param game			GameEnvironment object.
+	 * @param farm			The farm which the animal will be placed on.
 	 * @return					A string which states whether the animal purchase has been successfull or not.
 	 */
-	public String purchaseAnimal(Animal animal, GameEnvironment game) {
-		money = game.getFarm().getMoneyAvailable();
+	public String purchaseAnimal(Animal animal, Farm farm) {
+		money = farm.getMoneyAvailable();
 		if (money < animal.getAnimalPurchasePrice()){
 			message = "Sorry, You have insuffcient funds to purchase this animal";
 		}
 		else {
 			money = money - animal.getAnimalPurchasePrice();
-			game.getFarm().setMoneyAvailable(money);
-			game.getFarm().addAnimalToList(animal);
+			farm.setMoneyAvailable(money);
+			farm.addAnimalToList(animal);
 			message = "You have purchased " + animal.getAnimalName();
 		}
 		return message;
@@ -80,18 +80,18 @@ public class GeneralStore {
 	 * Purchasing a crop.
 	 * 
 	 * @param crop			The crop to be bought.
-	 * @param game			GameEnvironment object.
+	 * @param farm			The farm which the crop will be placed on.
 	 * @return					A string which states whether the crop purchase has been successfull or not.
 	 */
-	public String purchaseCrop(Crop crop, GameEnvironment game) {
-		money = game.getFarm().getMoneyAvailable();
+	public String purchaseCrop(Crop crop, Farm farm) {
+		money = farm.getMoneyAvailable();
 		if (money < crop.getCropPurchasePrice()){
 			message = "Sorry, You have insuffcient funds to purchase this crop";
 		}
 		else {
 			money = money - crop.getCropPurchasePrice();
-			game.getFarm().setMoneyAvailable(money);
-			game.getFarm().addCropToList(crop);
+			farm.setMoneyAvailable(money);
+			farm.addCropToList(crop);
 			message = "You have purchased one " + crop.getCropName();
 		}
 		return message;
