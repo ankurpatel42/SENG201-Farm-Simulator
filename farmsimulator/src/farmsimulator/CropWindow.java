af64ac37f752f90179607447d34f51d01a484d55
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-17 19:40:54 +1200

    code update (fixed days till harvest not updating)

--- farmsimulator/src/farmsimulator/CropWindow.java
+++ farmsimulator/src/farmsimulator/CropWindow.java
@@ -176,6 +176,7 @@ public class CropWindow{
 							itemListModel.removeElement(item);
 						}
 						JOptionPane.showMessageDialog(frmCrops, message);
+						frmCrops.repaint();
 					}
 				}
 			}
