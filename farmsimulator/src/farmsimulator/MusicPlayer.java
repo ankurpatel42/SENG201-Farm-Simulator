08f3a4c080fe85c55233283404a0ed69a989419b
ankurpatel42 <63583759+ankurpatel42@users.noreply.github.com>
2020-05-22 16:14:29 +1200

    updates

--- farmsimulator/src/farmsimulator/MusicPlayer.java
+++ farmsimulator/src/farmsimulator/MusicPlayer.java
@@ -1,6 +1,6 @@
 package farmsimulator;
 
-import java.io.File;
+import java.net.URL;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -8,7 +8,7 @@ import javax.sound.sampled.Clip;
 import javax.swing.JOptionPane;
 
 /**
- * This class contains the music that is to be played throughout the game, it loops continiously until the user has quit.
+ * This class contains the music that is to be played throughout the game, it loops continuously until the user has quit.
  * 
  * @author Ankur Patel, Benjamin Crozier
  * @version 15/05/2020
@@ -18,28 +18,22 @@ public class MusicPlayer {
 	
 	/**
 	 * The music to be played during the game.
-	 * 
-	 * @param filepath			The filepath/location of the .wav file which contains the audio to be played during the game.
+	 *
 	 */
-	public void playMusic(String filepath) {
+	public void playMusic() {
 		
 		try {
 			
-			File musicPath = new File(filepath);
-			
-			if (musicPath.exists()) {
-				AudioInputStream audio = AudioSystem.getAudioInputStream(musicPath);
-				Clip audioClip = AudioSystem.getClip();
-				audioClip.open(audio);
-				audioClip.start();
-				audioClip.loop(Clip.LOOP_CONTINUOUSLY);
-			}
-			else {
-				JOptionPane.showMessageDialog(null, "No sound file found.");
-			}
+			URL music = getClass().getResource("/music/Sergey_Cheremisinov_The_Healing.wav");
+
+			AudioInputStream audio = AudioSystem.getAudioInputStream(music);
+			Clip audioClip = AudioSystem.getClip();
+			audioClip.open(audio);
+			audioClip.start();
+			audioClip.loop(Clip.LOOP_CONTINUOUSLY);
 		}
 		catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Error playing music.");
+			JOptionPane.showMessageDialog(null, "Error playing music");
 		}
 	}
 }
